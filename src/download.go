7af2b03a6dd631b58507579ce9e14a81956c6290
Marcus Pereira <mpereirassa@gmail.com>
2020-01-24 14:39:28 -0300

    rename for download and remove getcurrentDir func

--- src/download.go
+++ src/download.go
@@ -2,23 +2,11 @@ package src
 
 import (
 	"log"
-	"os"
 
 	git "gopkg.in/src-d/go-git.v4"
 )
 
 var repoURL = "https://github.com/spadevs/flask_minimal"
-
-// GetCurrentDir return current directory
-func GetCurrentDir() string {
-	dir, err := os.Getwd()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return dir
-}
 
 // CloneRepo function clone repository from github
 func CloneRepo(currentPath string) {
