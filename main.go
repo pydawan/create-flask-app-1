71535b5e23fda481c56a3a9f512fd8efd1735f6c
Marcus Pereira <mpereirassa@gmail.com>
2020-01-24 14:47:09 -0300

    create func for get current Dir

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	"create-flask-app/src"
+	"log"
 	"os"
 	"path"
 )
 
+func getCurrentDir() string {
+	currentDir, err := os.Getwd()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return currentDir
+}
 func main() {
 	folderName := os.Args[1]
 	currentDir := src.GetCurrentDir()
