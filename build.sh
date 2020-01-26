ec6cc2c50735fce55d00804a73600338cc1f8235
Marcus Pereira <mpereirassa@gmail.com>
2020-01-26 15:52:28 -0300

    only 64bits

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
-GOOS=linux GOARCH=386 go build -o build/create-flask-app-for-linux-386 main.go
-GOOS=linux GOARCH=amd64 go build -o build/create-flask-app-for-linux-amd64 main.go
-GOOS=windows GOARCH=386 go build -o build/create-flask-app-for-windows-386.exe main.go
-GOOS=windows GOARCH=amd64 go build -o build/create-flask-app-for-windows-amd64.exe main.go
+GOOS=linux GOARCH=amd64 go build -o build/create-flask-app-for-linux main.go
+GOOS=windows GOARCH=amd64 go build -o build/create-flask-app.exe main.go
